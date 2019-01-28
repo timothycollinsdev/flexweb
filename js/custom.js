6be2fcbbb805b13b2c880c5bacc1e6755779166c
Timothy Collins <timothycollins.sean@gmail.com>
2019-01-28 22:08:39 +0530

    fix ux design issues - removed unneccesary flex web header from all pages except index.html

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,16 @@
 // javascript Animate onscroll Start
-    $(document).ready(function () {
-    if (screen.width > 1024) {
+$(document).ready(function() {
+  if (screen.width > 1024) {
     AOS.init({
-    easing: 'ease-in-out-sine',
-    once: true,
+      easing: "ease-in-out-sine",
+      once: true
     });
-    }
-    });
-(new IntersectionObserver(function(e,o){
-    if (e[0].intersectionRatio > 0){
-        document.documentElement.removeAttribute('class');
-    } else {
-        document.documentElement.setAttribute('class','stuck');
-    };
-})).observe(document.querySelector('.trigger'));
+  }
+});
+new IntersectionObserver(function(e, o) {
+  if (e[0].intersectionRatio > 0) {
+    document.documentElement.removeAttribute("class");
+  } else {
+    document.documentElement.setAttribute("class", "stuck");
+  }
+}).observe(document.querySelector(".trigger"));
