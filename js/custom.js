d47cc8dcce2dbd5917ee672cd767af3c4258328a
Timothy Collins <timothycollins.sean@gmail.com>
2019-02-07 19:33:02 +0530

    impl contact us form

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,13 @@
+function getFormData($form) {
+  var unindexed_array = $form.serializeArray();
+  var indexed_array = {};
+
+  $.map(unindexed_array, function(n, i) {
+    indexed_array[n["name"]] = n["value"];
+  });
+
+  return indexed_array;
+}
 // javascript Animate onscroll Start
 $(document).ready(function() {
   if (screen.width > 1024) {
@@ -6,6 +16,32 @@ $(document).ready(function() {
       once: true
     });
   }
+
+  $("#btnSubmit").click(e => {
+    e.preventDefault();
+    let values = getFormData($("#contactForm"));
+    if (!values.email || !values.message) {
+      return;
+    }
+
+    fetch("https://flexweb.glitch.me/contact", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(() => {
+        console.log("ok");
+        alert("Thank you for contacting us. We'll contact you soon.");
+      })
+      .catch(() => {
+        alert(
+          "There's some issue with submitting your query. Please contact us directly at support@flexweb.in"
+        );
+      });
+  });
 });
 new IntersectionObserver(function(e, o) {
   if (e[0].intersectionRatio > 0) {
